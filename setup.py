6d652f468808b71c44fbb13f5672d334906c641b
dengyong <dengyong@hugobiotech.com>
2022-11-07 15:31:37 +0800

    update install

--- setup.py
+++ setup.py
@@ -4,77 +4,102 @@ import sysconfig
 from setuptools import setup
 from setuptools.extension import Extension
 
-PKG = "hsync"
 
+class Packages(object):
 
-def get_version():
-    v = {}
-    with open("src/_version.py") as fi:
-        c = fi.read()
-    exec(compile(c, "src/_version.py", "exec"), v)
-    return v["__version__"]
+    def __init__(self, pkg_name=""):
+        self.name = pkg_name
+        self.base_dir = os.path.dirname(__file__)
+        self.source_dir = os.path.join(self.base_dir, "src")
+        self.version_file = os.path.join(self.base_dir, "src/_version.py")
+        self.des_file = os.path.join(self.base_dir, "README.md")
+        self.req_file = os.path.join(self.base_dir, "requirements.txt")
 
-
-def listdir(path):
-    df = []
-    for a, b, c in os.walk(path):
-        if os.path.basename(a).startswith("__"):
-            continue
-        for i in c:
-            if i.startswith("__") or i.endswith(".ini"):
+    @property
+    def listdir(self):
+        df = []
+        for a, b, c in os.walk(self.source_dir):
+            if os.path.basename(a).startswith("__"):
                 continue
-            p = os.path.join(a, i)
-            df.append(p)
-    return df
+            for i in c:
+                if i.startswith("__") or i.endswith(".ini"):
+                    continue
+                p = os.path.join(a, i)
+                df.append(p)
+        return df
 
+    @property
+    def description(self):
+        des = ""
+        with open(self.des_file) as fi:
+            des = fi.read()
+        return des
 
-def getExtension():
-    extensions = []
-    for f in listdir("src"):
-        e = Extension(PKG + "." + os.path.splitext(os.path.basename(f))[0],
-                      [f, ], extra_compile_args=["-O3", ],)
-        e.cython_directives = {
-            'language_level': sysconfig._PY_VERSION_SHORT_NO_DOT[0]}
-        extensions.append(e)
-    return extensions
+    @property
+    def version(self):
+        v = {}
+        with open(self.version_file) as fi:
+            c = fi.read()
+        exec(compile(c, self.version_file, "exec"), v)
+        return v["__version__"]
 
+    @property
+    def requirements(self):
+        requires = []
+        with open(self.req_file) as fi:
+            for line in fi:
+                line = line.strip()
+                requires.append(line)
+        return requires
 
-def getdes():
-    des = ""
-    with open(os.path.join(os.getcwd(), "README.md")) as fi:
-        des = fi.read()
-    return des
+    @property
+    def _extensions(self):
+        exts = []
+        for f in self.listdir:
+            e = Extension(self.name + "." + os.path.splitext(os.path.basename(f))[0],
+                          [f, ], extra_compile_args=["-O3", ],)
+            e.cython_directives = {
+                'language_level': sysconfig._PY_VERSION_SHORT_NO_DOT[0]}
+            exts.append(e)
+        return exts
 
+    def install(self, ext=False):
+        kwargs = {}
+        kwargs.update(
+            name=self.name,
+            version=self.version,
+            packages=[self.name, ],
+            package_data={self.name: ["*.ini", ]},
+            license="MIT",
+            url="https://github.com/yodeng/hsync",
+            package_dir={self.name: os.path.basename(self.source_dir)},
+            install_requires=self.requirements,
+            python_requires='>=3.8',
+            long_description=self.description,
+            long_description_content_type='text/markdown',
+            entry_points={'console_scripts': self._entrys},
+        )
+        if ext:
+            kwargs.pop("package_dir")
+            kwargs["ext_modules"] = self._extensions
+        setup(**kwargs)
 
-def get_requirement():
-    requires = []
-    with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as fi:
-        for line in fi:
-            line = line.strip()
-            requires.append(line)
-    return requires
-
-
-setup(
-    name=PKG,
-    version=get_version(),
-    packages=[PKG],
-    package_data={"hsync": ["*.ini", ]},
-    license="MIT",
-    url="https://github.com/yodeng/hget",
-    package_dir={PKG: "src"},
-    install_requires=get_requirement(),
-    python_requires='>=3.8',
-    # ext_modules=getExtension(),
-    long_description=getdes(),
-    long_description_content_type='text/markdown',
-    entry_points={
-        'console_scripts': [
+    @property
+    def _entrys(self):
+        eps = [
             'hsyncd = hsync.hsyncd:main',
             'hsync-keygen = hsync.hsyncd:create_hsync_keys',
-            'hscp = %s.%s:hscp_main' % (PKG, PKG),
-            'hsync = %s.%s:hsync_main' % (PKG, PKG),
+            'hscp = %s.%s:hscp_main' % (self.name, self.name),
+            'hsync = %s.%s:hsync_main' % (self.name, self.name),
             'hsync-echo-config = hsync.hsync:echo_config',
         ]
-    }
-)
+        return eps
+
+
+def main():
+    pkgs = Packages("hsync")
+    pkgs.install()
+
+
+if __name__ == "__main__":
+    main()
